d8f0c0c286dd5ca3fc6b4eac3dfedf4ad5c9ecf3
Ryan Luong <mail.lcvinh@gmail.com>
2025-09-20 03:46:09 +0000

    feature_ryanLuong/setup_environment

--- README.md
+++ README.md
@@ -67,4 +67,3 @@ export default defineConfig([
   },
 ])
 ```
-# simpleWebBlog_FE
--- eslint.config.js
+++ eslint.config.js
@@ -19,5 +19,25 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      'no-console': 'warn',
+      'no-lonely-if': 'warn',
+      'no-unused-vars': 'warn',
+      'no-trailing-spaces': 'warn',
+      'no-multi-spaces': 'warn',
+      'no-multiple-empty-lines': 'warn',
+      'space-before-blocks': ['error', 'always'],
+      'object-curly-spacing': ['warn', 'always'],
+      'indent': ['warn', 2],
+      'semi': ['error', 'never'],
+      'quotes': ['error', 'single'],
+      'array-bracket-spacing': 'warn',
+      'linebreak-style': 'off',
+      'no-unexpected-multiline': 'warn',
+      'keyword-spacing': 'warn',
+      'comma-dangle': 'warn',
+      'comma-spacing': 'warn',
+      'arrow-spacing': 'warn'
+    }
   },
 ])
